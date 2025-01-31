8e9da7ac42208c9ed3c7f4855a6098d23c871722
Lopes0908 <lucas.lopes052011@gmail.com>
2025-01-30 22:06:15 -0400

    Dirose v 0.0.4

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
   <p>Entre em contato para adquirir as peças: (67) 93505-8192</p>
   
   <div>
-    <button on:click={() => window.location.href='/categoria'} style="background-color: #b76e79; color: white;">Categoria 1</button>
+    <button on:click={() => window.location.href='/categoria1'} style="background-color: #b76e79; color: white;">Categoria 1</button>
     <button on:click={() => window.location.href='/categoria2'} style="background-color: #b76e79; color: white;">Categoria 2</button>    
   </div>
 </main>
