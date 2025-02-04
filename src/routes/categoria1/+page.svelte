b3bfa7ce615478b757a9b64a4021ff2e554a2923
Lopes0908 <lucas.lopes052011@gmail.com>
2025-02-04 16:24:59 -0400

    Sexta versão

--- src/routes/categoria1/+page.svelte
+++ src/routes/categoria1/+page.svelte
@@ -1,1 +1,2 @@
-ola
+<button on:click={() => window.location.href='/categoria1'} style="color:black">Peças</button>
+<button on:click={() => window.location.href='/categoria2'} style="color:black">Peças Masculinas</button>
